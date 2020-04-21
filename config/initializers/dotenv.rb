bc4fdbff06f2517d567de90373b66266a3537b91
Eugene Zaretskiy <ujinius@gmail.com>
2020-04-21 14:23:32 +0300

    - adjusted dotenv for dev, test modes

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,5 +1,9 @@
-Dotenv.require_keys(
-  'CONTENTFUL_ACCESS_TOKEN',
-  'CONTENTFUL_SPACE_ID',
-  'CONTENTFUL_ENVIRONMENT'
-)
+require 'dotenv'
+
+unless Rails.env.production?
+  Dotenv.require_keys(
+    'CONTENTFUL_ACCESS_TOKEN',
+    'CONTENTFUL_SPACE_ID',
+    'CONTENTFUL_ENVIRONMENT'
+  )
+end
